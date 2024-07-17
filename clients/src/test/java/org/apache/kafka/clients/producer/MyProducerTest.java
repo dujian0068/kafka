cad342586dc34b25f11301e2869740ad7c78690b
dujian0068 <1426703092@qq.com>
2024-07-17 21:53:34 +0800

    17070 test

--- clients/src/test/java/org/apache/kafka/clients/producer/MyProducerTest.java
+++ clients/src/test/java/org/apache/kafka/clients/producer/MyProducerTest.java
@@ -6,24 +6,23 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.util.Arrays;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-@BenchmarkMode(Mode.AverageTime)
-@State(Scope.Thread)
-@Fork(1)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Warmup(iterations = 3)
-@Measurement(iterations = 5)
+//@BenchmarkMode(Mode.AverageTime)
+//@State(Scope.Thread)
+//@Fork(1)
+//@OutputTimeUnit(TimeUnit.MILLISECONDS)
+//@Warmup(iterations = 3)
+//@Measurement(iterations = 5)
 public class MyProducerTest {
 
     public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder()
-                .include(MyProducerTest.class.getSimpleName())
-                .build();
-        new Runner(opt).run();
+        MyProducerTest myProducerTest = new MyProducerTest();
 
+        myProducerTest.send();
     }
     static String bootstrapServers = "localhost:9092"; // 替换为你的Kafka服务器地址
     static String topic = "JMH_TOPIC"; // 替换为你的主题名称
@@ -37,15 +36,75 @@ public class MyProducerTest {
         properties.put("key.serializer", org.apache.kafka.common.serialization.StringSerializer.class);
         properties.put("value.serializer", org.apache.kafka.common.serialization.StringSerializer.class);
         producer = new KafkaProducer<>(properties);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         String key = UUID.randomUUID().toString();
-        record = new ProducerRecord<>(topic, key, key);
+        record = new ProducerRecord<>(topic, key, UUID.randomUUID().toString()+
+                UUID.randomUUID().toString()+
+                UUID.randomUUID().toString()+
+                UUID.randomUUID().toString()+
+                UUID.randomUUID().toString()+
+                UUID.randomUUID().toString());
     }
 
-    @Benchmark
+    //@Benchmark
     public void send(){
-
-        for (int i = 0; i< 10000000  ; i++){
-            producer.send(record);
+        long[] time = new long[50];
+        for (int j = 0;j<50; j++){
+            long start = System.currentTimeMillis();
+            for (int i = 0; i< 1000000; i++){
+                producer.send(record);
+            }
+            long end = System.currentTimeMillis();
+            time[j] = end - start;
         }
+        System.out.println(Arrays.toString(time));
+        System.out.println(average(time));
+        System.out.println(standardDiviation(time));
+
+
+    }
+
+    /**
+     * 传入一个数列x计算平均值
+     * @param x
+     * @return 平均值
+     */
+    public static double average(long[] x) {
+        int n = x.length;            //数列元素个数
+        double sum = 0;
+        for (long i : x) {        //求和
+            sum+=i;
+        }
+        return sum/n;
+    }
+
+    /**
+     * 传入一个数列x计算方差
+     * 方差s^2=[（x1-x）^2+（x2-x）^2+......（xn-x）^2]/（n）（x为平均数）
+     * @param x 要计算的数列
+     * @return 方差
+     */
+    public static double variance(long[] x) {
+        int n = x.length;            //数列元素个数
+        double avg = average(x);    //求平均值
+        double var = 0;
+        for (double i : x) {
+            var += (i-avg)*(i-avg);    //（x1-x）^2+（x2-x）^2+......（xn-x）^2
+        }
+        return var/n;
+    }
+
+    /**
+     * 传入一个数列x计算标准差
+     * 标准差σ=sqrt(s^2)，即标准差=方差的平方根
+     * @param x 要计算的数列
+     * @return 标准差
+     */
+    public static double standardDiviation(long[] x) {
+        return  Math.sqrt(variance(x));
     }
 }
